735120d52554c757bc7bc7f77ee1d27bf1d85a9c
jamesbieber <jamesbieber97@gmail.com>
2019-12-10 20:44:59 -0800

    Stock prices working

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,29 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batch = 0
+    more = True  # keep track of extra ingredients
+
+    while more:
+        for k, v in ingredients.items():
+            if v - recipe[k] < 0:
+                return batch
+            else:
+                v -= recipe[k]
+                if v - recipe[k] < 0:
+                    more = False
+
+        batch += 1
+
+    return batch
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,27 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    smallest = prices[0]
+    biggest = prices[1] - prices[0]
+
+    for i in range(1, len(prices)):
+        if prices[i] < smallest:
+            smallest = prices[i]
+        elif prices[i] - smallest > biggest:
+            biggest = prices[i] - smallest
+
+    return biggest
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+        # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
